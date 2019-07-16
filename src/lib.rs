cf5c963d030d0e17076f8f879f074572b0c7bb96
ogkloo <no@email.net>
2019-07-16 15:54:31 -0700

    Makes stumbling incremental progress to figuring out ncollide3d

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
 use nalgebra::Vector3;
+use ncollide3d::math::Point;
+use ncollide3d::query::Ray;
+
 /// Defines a viewport for a specific eye node
 /// with the eye node as the origin.
+/// FoV is in radians.
+#[derive(Debug)]
 pub struct Viewport {
-    eye: Vector3<f64>,
-    up: Vector3<f64>,
+    point: Point<f64>,
+    eye: Ray<f64>,
+    up: Ray<f64>,
     fov: f64,
     dimensions: (u64, u64),
     depth: u64,
 }
 
 impl Viewport {
-    pub fn new(e: Vector3<f64>, u: Vector3<f64>, f: f64, dim: (u64, u64), dep: u64) -> Viewport {
+    pub fn new(
+        p: Point<f64>,
+        e: Ray<f64>,
+        u: Ray<f64>,
+        f: f64,
+        dim: (u64, u64),
+        dep: u64,
+    ) -> Viewport {
         Viewport {
+            point: p,
             eye: e,
             up: u,
             fov: f,
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
     use nalgebra::Vector3;
+    use ncollide3d::math::Point;
+    use ncollide3d::query::Ray;
     use raytrace::*;
     let view = Viewport::new(
-        Vector3::new(1.0, 1.0, 1.0),
-        Vector3::new(0.0, 0.0, 1.0),
+        Point::new(0.0, 0.0, 0.0),
+        Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(1.0, 1.0, 1.0)),
+        Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 1.0)),
         std::f64::consts::PI / 2.0,
         (1920, 1080),
         1000,
     );
+    println!("{:?}", view);
     println!("Hello, world!");
 }
