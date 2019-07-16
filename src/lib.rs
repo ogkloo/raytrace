fc019d6368514bd7f32095c7b35588749b1ad0b4
ogkloo <no@email.net>
2019-07-16 12:09:41 -0700

    Adds new function for viewport

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
 use nalgebra::Vector3;
-pub type Ray = Vector3<f64>;
 /// Defines a viewport for a specific eye node
 /// with the eye node as the origin.
 pub struct Viewport {
-    eye: Ray,
-    up: Ray,
+    eye: Vector3<f64>,
+    up: Vector3<f64>,
     fov: f64,
     dimensions: (u64, u64),
     depth: u64,
 }
 
-impl Viewport {}
+impl Viewport {
+    pub fn new(e: Vector3<f64>, u: Vector3<f64>, f: f64, dim: (u64, u64), dep: u64) -> Viewport {
+        Viewport {
+            eye: e,
+            up: u,
+            fov: f,
+            dimensions: dim,
+            depth: dep,
+        }
+    }
+}
 
 pub enum Polyhedron {}
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
+    use nalgebra::Vector3;
     use raytrace::*;
-    let view = Viewport {
-        eye: Ray::new(1.0, 1.0, 1.0),
-        up: Ray::new(0.0, 0.0, 1.0),
-        fov: std::f64::consts::PI / 2.0,
-        dimensions: (1920, 1080),
-        depth: 1000,
-    };
+    let view = Viewport::new(
+        Vector3::new(1.0, 1.0, 1.0),
+        Vector3::new(0.0, 0.0, 1.0),
+        std::f64::consts::PI / 2.0,
+        (1920, 1080),
+        1000,
+    );
     println!("Hello, world!");
 }
