9985200f307e28bd79857fdf951b86c501139837
ogkloo <no@email.net>
2019-07-16 11:55:46 -0700

    Adds the warnings back in and also adds the main

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,17 @@
-pub mod raytrace {
-    use nalgebra::{Matrix, VecStorage, U1, U3};
-    pub type Ray = Matrix<f64, U3, U1, VecStorage<f64, U3, U1>>;
-
-    /// Defines a viewport for a specific eye node
-    /// with the eye node as the origin.
-    pub struct Viewport {
-        pub eye: Ray,
-        pub up: Ray,
-        pub fov: f64,
-        pub dimensions: (u64, u64),
-        pub depth: u64,
-    }
-
-    impl Viewport {}
-
-    pub enum Polyhedron {}
-
-    pub struct Scene {}
+use nalgebra::Vector3;
+pub type Ray = Vector3<f64>;
+/// Defines a viewport for a specific eye node
+/// with the eye node as the origin.
+pub struct Viewport {
+    eye: Ray,
+    up: Ray,
+    fov: f64,
+    dimensions: (u64, u64),
+    depth: u64,
 }
+
+impl Viewport {}
+
+pub enum Polyhedron {}
+
+pub struct Scene {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
+    use raytrace::*;
+    let view = Viewport {
+        eye: Ray::new(1.0, 1.0, 1.0),
+        up: Ray::new(0.0, 0.0, 1.0),
+        fov: std::f64::consts::PI / 2.0,
+        dimensions: (1920, 1080),
+        depth: 1000,
+    };
     println!("Hello, world!");
 }
