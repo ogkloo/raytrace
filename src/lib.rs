0e41699e7a70082b7aee6438ac3f78b6acced5bb
no <no>
2019-07-15 23:05:03 -0700

    Gets rustfmt to shut up about dead, better lays out skeleton.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-use nalgebra::{Matrix, VecStorage, U1, U3};
-type Ray = Matrix<f64, U3, U1, VecStorage<f64, U3, U1>>;
+pub mod raytrace {
+    use nalgebra::{Matrix, VecStorage, U1, U3};
+    pub type Ray = Matrix<f64, U3, U1, VecStorage<f64, U3, U1>>;
 
-/// Defines a viewport for a specific eye node
-/// with the eye node as the origin.
-pub struct Viewport {
-    eye: Ray,
-    fov: f64,
-    dimensions: (u64, u64),
-    up: Ray,
-    depth: u64,
+    /// Defines a viewport for a specific eye node
+    /// with the eye node as the origin.
+    pub struct Viewport {
+        pub eye: Ray,
+        pub up: Ray,
+        pub fov: f64,
+        pub dimensions: (u64, u64),
+        pub depth: u64,
+    }
+
+    impl Viewport {}
+
+    pub enum Polyhedron {}
+
+    pub struct Scene {}
 }
-
-impl Viewport {}
-
-pub enum Polyhedron {}
-
-pub struct Scene {}
