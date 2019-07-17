af2f5f3ebd019c30d9bf45f0dea2868dce344376
ogkloo <no@email.net>
2019-07-16 17:57:33 -0700

    Adds draw_ray function which checks intersections, notes, and a Polyhedron struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
+use nalgebra::geometry::Isometry3;
 use nalgebra::Vector3;
 use ncollide3d::math::Point;
 use ncollide3d::query::Ray;
+use ncollide3d::query::RayCast;
 
 /// Defines a viewport for a specific eye node
 /// with the eye node as the origin.
 /// FoV is in radians.
 #[derive(Debug)]
 pub struct Viewport {
-    point: Point<f64>,
+    position: Point<f64>,
     eye: Ray<f64>,
     up: Ray<f64>,
     fov: f64,
@@ -15,26 +17,44 @@ pub struct Viewport {
     depth: u64,
 }
 
+/// A shape and its material properties
+/// Other properties go here as we progress
+pub struct Polyhedron<T: RayCast<f64>> {
+    shape: T,
+    color: image::Rgb<u64>,
+}
+
 impl Viewport {
+    /// Constructor
     pub fn new(
         p: Point<f64>,
-        e: Ray<f64>,
-        u: Ray<f64>,
+        e: Vector3<f64>,
+        u: Vector3<f64>,
         f: f64,
         dim: (u64, u64),
         dep: u64,
     ) -> Viewport {
         Viewport {
-            point: p,
-            eye: e,
-            up: u,
+            position: p,
+            eye: Ray::new(p, e),
+            up: Ray::new(p, u),
             fov: f,
             dimensions: dim,
             depth: dep,
         }
     }
-}
 
-pub enum Polyhedron {}
+    /// Draws a ray at a certain angle and returns the color
+    /// of whatever it intersects. Will later draw recursively.
+    // Maybe it should actually just place them in a big pixel buffer, or maybe this should be
+    // called by a private method that does that.
+    pub fn draw_ray<T: RayCast<f64>>(ray: Ray<f64>, object: T) -> Option<image::Rgb<u64>> {
+        if object.intersects_ray(&Isometry3::identity(), &ray) {
+            Some(image::Rgb([0, 0, 0]))
+        } else {
+            None
+        }
+    }
+}
 
 pub struct Scene {}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     use nalgebra::Vector3;
     use ncollide3d::math::Point;
-    use ncollide3d::query::Ray;
     use raytrace::*;
     let view = Viewport::new(
         Point::new(0.0, 0.0, 0.0),
-        Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(1.0, 1.0, 1.0)),
-        Ray::new(Point::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.0, 1.0)),
+        Vector3::new(1.0, 1.0, 1.0),
+        Vector3::new(0.0, 0.0, 1.0),
         std::f64::consts::PI / 2.0,
         (1920, 1080),
         1000,
