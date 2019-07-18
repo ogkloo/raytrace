da63acae9df2d5ecb9d2ab788ac8184e6c34e08f
ogkloo <no@email.net>
2019-07-17 19:26:33 -0700

    Adds scene implementation and a basic struct for it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use image::ImageBuffer;
 use nalgebra::geometry::Isometry3;
 use nalgebra::Vector3;
 use ncollide3d::math::Point;
@@ -13,17 +14,8 @@ pub struct Viewport {
     eye: Ray<f64>,
     up: Ray<f64>,
     fov: f64,
-    dimensions: (u64, u64),
+    pub dimensions: (u32, u32),
     depth: u64,
-}
-
-/// A shape and its material properties
-/// Other properties go here as we progress
-pub struct Polyhedron<T: RayCast<f64>> {
-    shape: T,
-    color: image::Rgb<u64>,
-    reflectivity: f64,
-    refractivity: f64,
 }
 
 impl Viewport {
@@ -33,9 +25,9 @@ impl Viewport {
         e: Vector3<f64>,
         u: Vector3<f64>,
         f: f64,
-        dim: (u64, u64),
+        dim: (u32, u32),
         dep: u64,
-    ) -> Viewport {
+    ) -> Self {
         Viewport {
             position: p,
             eye: Ray::new(p, e),
@@ -50,16 +42,50 @@ impl Viewport {
     /// of whatever it intersects. Will later draw recursively.
     // Maybe it should actually just place them in a big pixel buffer, or maybe this should be
     // called by a private method that does that.
-    pub fn draw_ray<T: RayCast<f64>>(
+    pub fn draw_ray<R: RayCast<f64>>(
         ray: &Ray<f64>,
-        object: &Polyhedron<T>,
-    ) -> Option<image::Rgb<u64>> {
+        object: &Polyhedron<R>,
+    ) -> Option<image::Rgb<u8>> {
         if object.shape.intersects_ray(&Isometry3::identity(), &ray) {
-            Some(image::Rgb([0, 0, 0]))
+            Some(object.color)
         } else {
             None
         }
     }
 }
 
-pub struct Scene {}
+/// A shape and its material properties
+/// Other properties go here as we progress
+pub struct Polyhedron<R: RayCast<f64>> {
+    shape: R,
+    color: image::Rgb<u8>,
+    // reflectivity: f64,
+    // refractivity: f64,
+}
+
+pub struct Scene<R: RayCast<f64>> {
+    // TODO
+    // Huge problem: Only one type in a scene I think?
+    objects: Vec<Polyhedron<R>>,
+    camera: Viewport,
+}
+
+impl<R: RayCast<f64>> Scene<R> {
+    pub fn new(objs: Vec<Polyhedron<R>>, eye: Viewport) -> Self {
+        Scene::<R> {
+            objects: objs,
+            camera: eye,
+        }
+    }
+
+    pub fn render(&self, filename: String) {
+        let mut img: image::RgbImage =
+            ImageBuffer::new(self.camera.dimensions.0, self.camera.dimensions.1);
+        for (_x, _y, pixel) in img.enumerate_pixels_mut() {
+            for object in self.objects.iter() {
+                *pixel = Viewport::draw_ray(&self.camera.eye, &object).unwrap();
+            }
+        }
+        img.save(filename).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ fn main() {
     use nalgebra::Vector3;
     use ncollide3d::math::Point;
     use raytrace::*;
+    let xsize = 1920;
+    let ysize = 1080;
     let view = Viewport::new(
         Point::new(0.0, 0.0, 0.0),
         Vector3::new(1.0, 1.0, 1.0),
         Vector3::new(0.0, 0.0, 1.0),
         std::f64::consts::PI / 2.0,
-        (1920, 1080),
+        (xsize, ysize),
         1000,
     );
     println!("{:?}", view);
-    println!("Hello, world!");
 }
